eef51e5ecb0b6be07df170ec43099f3e51459bc7
Sergei Nozdrenkov <nozdrenkov@gmail.com>
2024-09-22 23:22:26 +0100

    Use Tailwind instead of CSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
 // tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html", "./**/*.{html,js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
