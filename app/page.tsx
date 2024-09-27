af32a577279384b08e440d2404d9ccdbb5986af0
Sergei Nozdrenkov <nozdrenkov@gmail.com>
2024-09-27 22:47:22 +0100

    make wildflow logo clickable

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,25 @@
 
 import Viewer3DWrapper from "./components/Viewer3DWrapper";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="relative w-full h-screen">
       <Viewer3DWrapper />
       <div className="absolute bottom-2.5 left-2.5 z-50">
-        <Image src="/favicon.svg" alt="Wildflow Logo" width={30} height={30} />
+        <Link
+          href="https://wildflow.ai"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <Image
+            src="/favicon.svg"
+            alt="Wildflow Logo"
+            width={30}
+            height={30}
+          />
+        </Link>
       </div>
       <div className="absolute bottom-0 right-0 z-50 bg-black/70 px-2.5 py-1 text-white font-poppins text-sm leading-[1.4] text-right">
         Data: UCL, LU, IPB...
