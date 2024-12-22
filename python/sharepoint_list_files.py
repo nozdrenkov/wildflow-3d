a44c49728f4b8c48a5d879119198795edddb19ac
Sergei Nozdrenkov <nozdrenkov@gmail.com>
2024-12-21 19:55:03 -0500

    even better

--- python/sharepoint_list_files.py
+++ python/sharepoint_list_files.py
@@ -13,18 +13,19 @@ def extract_id_from_url(url):
 
   return url[start_index:end_index]
 
-def get_sharepoint_files():
+def create_request(order_asc=True):
     url = 'https://livelancsac.sharepoint.com/sites/Grp-MSSSpermonderestorationfieldwork/_api/web/GetListUsingPath(DecodedUrl=@a1)/RenderListDataAsStream'
     
     params = {
         '@a1': "'/sites/Grp-MSSSpermonderestorationfieldwork/Shared Documents'",
+        'SortField' : 'LinkFilename',
+        'SortDir' : ('Asc' if order_asc else 'Desc'),
         'TryNewExperienceSingle': 'TRUE',
-        'Paged': 'TRUE',
+        # 'Paged': 'TRUE',
         'p_FileLeafRef': 'GP_Right (1684).JPG',
-        # 'p_FileLeafRef': 'GP_Right (1595).JPG',
         'p_ID': '705404',
         'RootFolder': '/sites/Grp-MSSSpermonderestorationfieldwork/Shared Documents/General/Photogrammetry 2024/50mx20m plots/Photos + Metashape projects/M7_Salisi_Kecil_Healthy_26.06.2024/M7_Salisi_Kecil_Healthy_26.06.2024_photos',
-        'ix_Paged': 'TRUE',
+        # 'ix_Paged': 'TRUE',
         'ix_ID': '705404',
         'ix_Key': '01OBEEJLNN24VT5SJFKJC3VUQGGAML44WJ',
         'PageFirstRow': '5100',
@@ -37,7 +38,7 @@ def get_sharepoint_files():
         'application': 'sp_files',
         'authorization': 'Bearer',  # Add your actual bearer token here
         'content-type': 'application/json;odata=verbose',
-        'cookie': 'MSFPC=GUID=7b2feaa9a1bb4b5283031ec4e8fc352e&HASH=7b2f&LV=202405&V=4&LU=1717070853539; FeatureOverrides_experiments=[]; WordWacDataCenter=GUK3; WordWacDataCenterSetTime=2024-12-21T00:34:26.174Z; ExcelWacDataCenter=PUS9; WacDataCenter=PUS9; ExcelWacDataCenterSetTime=2024-12-21T01:25:45.184Z; WacDataCenterSetTime=2024-12-21T01:25:45.184Z; WSS_FullScreenMode=false; MicrosoftApplicationsTelemetryDeviceId=52cf9f05-ce67-47b1-a976-556eeece3f31; FedAuth=77u/PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48U1A+VjEzLDBoLmZ8bWVtYmVyc2hpcHx1cm4lM2FzcG8lM2FndWVzdCNub3pkcmVua292QGdtYWlsLmNvbSwwIy5mfG1lbWJlcnNoaXB8dXJuJTNhc3BvJTNhZ3Vlc3Qjbm96ZHJlbmtvdkBnbWFpbC5jb20sMTMzNzkyMTM4MzMwMDAwMDAwLDAsMTMzNzkzNzg5NDYyMDc1NzI5LDAuMC4wLjAsMTAyNiw5YzliY2QxMS05NzdhLTRlOWMtYTlhMC1iYzczNDA5MDE2NGEsLCxiODhiNmZhMS01MGZiLWEwMDAtZjc3Ni0wZmNmMjM4ZjQxNzgsMTNkNzZmYTEtZjA0OC1hMDAwLWY3NzYtMDMxY2NlZDliMjViLGNDc2xWVVBNV1V5WWpKR2d5RURqeHcsMTMzNzkyMTM1Mzk4NzU3MjIzLDAsMCwsLCwyNjUwNDY3NzQzOTk5OTk5OTk5LDAsLCwsLCwsMCwsMTg5OTY2LHVYZWhRSlBsZVZqTkNiYWtVaEdENkl5RlFRayxJb2ZrMjhWWmlkb21EOHk2emlobWtGNzBCMUJMdWJNR201K2Q5M0pZU2FYWUlBSnpCcVh6MzZiYVQ1S1loWXJ4REIxYXRMdXYrOUd0eE9scnpCWmFkZjJjMTVjUkpDQzZ1VlcvMkxkK0pFRTB1T1RKVHpYOGwvZkhFd3Y2aU1kTWp1UnlmZ0RhZlZ0QURFOVdyRmVNeFNmcXpGdUpGd1dJdlVvcmhBaHQ4M1VpOUMxanRDZEJkWnpidm1BR053dnlhWnJYaXhZWGU5dnJjWDFqbjdhbnJVMmlWVFp4MWNYamJjTTdtNjMrcXYxWkw4NjdPSEovN1FVMW4rNnJKZUhKcTR1ZVNMM2NIZVdYc0grVXdkTG80b2JSaE5lVmNjdmJTSG5KVU9UaXliakJRYlo3QXNaby9XSUh0dk9QdERzUlJQZ25nVzJzNUVVajAwdXJuVVo0S0E9PTwvU1A+',
+        'cookie': 'MSFPC=GUID=7b2feaa9a1bb4b5283031ec4e8fc352e&HASH=7b2f&LV=202405&V=4&LU=1717070853539; FeatureOverrides_experiments=[]; WordWacDataCenter=GUK3; WordWacDataCenterSetTime=2024-12-21T00:34:26.174Z; ExcelWacDataCenter=PUS9; WacDataCenter=PUS9; ExcelWacDataCenterSetTime=2024-12-21T01:25:45.184Z; WacDataCenterSetTime=2024-12-21T01:25:45.184Z; WSS_FullScreenMode=false; MicrosoftApplicationsTelemetryDeviceId=52cf9f05-ce67-47b1-a976-556eeece3f31; FedAuth=77u/PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiPz48U1A+VjEzLDBoLmZ8bWVtYmVyc2hpcHx1cm4lM2FzcG8lM2FndWVzdCNub3pkcmVua292QGdtYWlsLmNvbSwwIy5mfG1lbWJlcnNoaXB8dXJuJTNhc3BvJTNhZ3Vlc3Qjbm96ZHJlbmtvdkBnbWFpbC5jb20sMTMzNzkyMTM4MzMwMDAwMDAwLDAsMTMzNzkzNzg5NDYyMDc1NzI5LDAuMC4wLjAsMTAyNiw5YzliY2QxMS05NzdhLTRlOWMtYTlhMC1iYzczNDA5MDE2NGEsLCxiODhiNmZhMS01MGZiLWEwMDAtZjc3Ni0wZmNmMjM4ZjQxNzgsMTNkNzZmYTEtZjA0OC1hMDAwLWY3NzYtMDMxY2NlZDliMjViLGNDc2xWVVBNV1V5WWpKR2d5RURqeHcsMTMzNzkyMTM1Mzk4NzU3MjIzLDAsMCwsLCwyNjUwNDY3NzQzOTk5OTk5OTk5LDAsLCwsLCwsMCwsMTg5OTY2LHVYZWhRSlBsZVZqTkNiYWtVaEdENkl5RlFRayxJb2ZrMjhWWmlkb21EOHk2emlobWtGNzBCMUJMdWJNR201K2Q5M0pZU2FYWUlBSnpCcVh6MzZiYVQ1S1loWXJ4REIxYXRMdXYrOUd0eE9scnpCWmFkZjJjMTVjUkpDQzZ1VlcvMkxkK0pFRTB1T1RKVHpYOGwvZkhFd3Y2aU1kTWp1UnlmZ0RhZlZ0QURFOVdyRmVNeFNmcXpGdUpGd1dJdlVvcmhBaHQ4M1VpOUMxanRDZEJkWnpidm1BR053dnlhWnJYaXhZWGU5dnJjWDFqbjdhbnJVMmlWVFp4MWNYamJjTTdtNjMrcXYxWkw4NjdPSEovN1FVMW4rNnJKZUhKcTR1ZVNMM2NIZVdYc0grVXdkTG80b2JSaE5lVmNjdmJTSG5KVU9UaXliakJRYlo3QXNaby9XSUh0dk9QdERzUlJQZ25nVzJzNUVVajAwdXJuVVo0S0E9PTwvU1A+; ai_session=xkIvQNPyI2siBgFcfTK5Uu|1734818945934|1734826411326',
         'origin': 'https://livelancsac.sharepoint.com',
         'referer': 'https://livelancsac.sharepoint.com/sites/Grp-MSSSpermonderestorationfieldwork/Shared%20Documents/Forms/AllItems.aspx',
         'x-clientservice-clienttag': 'SPList Web',
@@ -56,7 +57,9 @@ def get_sharepoint_files():
             "RequireFolderColoringFields": True
         }
     }
+    return url, params, headers, data
 
+def request_and_parse_response(url, params, headers, data):
     try:
         response = requests.post(url, params=params, headers=headers, json=data)
         response.raise_for_status()
@@ -80,10 +83,22 @@ def get_sharepoint_files():
         print(f"Error making request: {e}")
         return None
 
-# Example usage
+def get_sharepoint_files():
+    r1 = request_and_parse_response(*create_request(True))
+    r2 = request_and_parse_response(*create_request(False))
+    combined_files = r1 + r2
+    unique_files = []
+    seen = set()
+    for file in combined_files:
+        if file['file_id'] not in seen:
+            unique_files.append(file)
+            seen.add(file['file_id'])
+    return unique_files
+
 if __name__ == "__main__":
     files = get_sharepoint_files()
+    filename = 'files_2024.json'
     if files:
-        with open('files_2024_3.json', 'w') as file:
+        with open(filename, 'w') as file:
             json.dump(files, file)
-        print(f"{len(files)} files saved to files_2024.json")
+        print(f"{len(files)} files saved to {filename}")
