edbc1e8f03dcdb7ecaffd1038ec3e87b19388637
Sergei Nozdrenkov <nozdrenkov@gmail.com>
2025-09-05 15:29:59 +0100

    upd cors

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,20 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/(.*)",
+        source: "/o/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "unsafe-none",
+          },
+        ],
+      },
+      {
+        source: "/((?!o/).*)",
         headers: [
           {
             key: "Cross-Origin-Opener-Policy",
